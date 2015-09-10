de649eba8d8fe19c0a9f5d360c75c7d405940859
Bayarkhuu Purevgaram <bayarkhuu0512@gmail.com>
2015-09-10 11:44:08 +0900

    Init.
    Tested Animation Sprite

---
+++ main.lua
@@ -0,0 +1,52 @@
+local physics = require( "physics")
+physics.start( )
+
+
+local sheetData = {
+	width = 191,
+	height = 206,
+	numFrames = 7,
+	sheetContentWidth = 1337,
+	sheetContentHeight = 206
+}
+
+local mySheet = graphics.newImageSheet( "sprite_sheet.png", sheetData )
+
+local sequenceData ={
+	{
+		name = "normalRun",
+		start = 1,
+		count = 7,
+		time = 700
+	},
+	{
+		name = "fastRun",
+		frames = {1,2,3},
+		time = 250,
+		loopCount = 0
+	}
+
+}
+
+local animation = display.newSprite( mySheet, sequenceData )
+animation.x = display.contentWidth/2
+animation.y = display.contentHeight/2
+
+function animation:touch( event )
+	if (event.phase == "began") then
+		self.markX = self.x
+		self.markY = self.y
+		self:play()
+	elseif event.phase == "moved" then
+		local x = (event.x - event.xStart) +  self.markX
+		local y = (event.y - event.yStart) + self.markY
+		self.x, self.y = x,y
+	elseif event.phase == "ended" or event.phase == "cancelled" then
+		self:setFrame(0)
+		self:pause()	
+
+	end
+	return true
+end
+
+animation:addEventListener( "touch", animation )
